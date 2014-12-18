2871a563663d25c1a0a93a1c31976b8ebaf858a7
杨超竣 <1946231687@qq.com>
2014-12-18 09:38:45 +0800

    version 3.0

---
+++ include/info.hrl
@@ -0,0 +1,10 @@
+-define(RECEIVE_CLIENT_SUP,receive_client_sup).
+-define(RECEIVE_CLIENT_SERVER,receive_client_server).
+-define(CHAT_TAB_SERVER,chat_tab_server).
+-define(CHAT_TAB_SUP,chat_tab_sup).
+-define(SEND_CLIENT_SERVER,send_client_server).
+-define(SEND_CLIENT_SUP,send_client_sup).
+-define(CHAT_SERVER,chat_server3).
+-define(MSG_SERVER,msg_server).
+-define(LOGIN_TAG,"bG9naW4=").
+-define(SWITCH_TAG,"c3dpdGNo").
---
+++ src/server/info.hrl
@@ -0,0 +1,4 @@
+-define(CHAT_SERVER,chat_server3).
+-define(MSG_SERVER,msg_server).
+-define(LOGIN_TAG,"bG9naW4=").
+-define(SWITCH_TAG,"c3dpdGNo").
